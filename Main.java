81a79a6b844837abdf70b7bea1e4cf16d4653592
dac <dac@students.acts.in>
2024-10-17 09:36:43 +0530

    after docker run

--- Main.java
+++ Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
 
-        System.out.println("Enter first numbers");
+        System.out.println("enter a");
         int a = sc.nextInt();
 
-        System.out.println("Enter second number");
-        int b = sc.nextInt();
 
-        System.out.println("Enter third number");
-        int c = sc.nextInt();
+        int b = 5;
+
+
+        int c = 4;
 
         if ( a  > b && a > c ){
             System.out.println("a is greater");
